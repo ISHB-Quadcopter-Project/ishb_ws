4223362c94264817af87eb2a8d78479bca4d2b62
holybro6767-crypto <holybro6767@gmail.com>
2026-07-17 17:32:48 -0700

    Goes to pos 6.7 lol

--- scripts/ourNode.py
+++ scripts/ourNode.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
 
 def main():
     rospy.init_node("ourNode") #Make ourNode
+
+    #Intializing publisher
+    pub = rospy.Publisher("/super/goal", PoseStamped, queue_size = 10)
+
+    #Wait until get a connection
+    while pub.get_num_connections() == 0 and not rospy.is_shutdown():
+        rospy.sleep(0.1) #If no connection, sleep
+
+    #Creating bare bone message to publish
+    msg = PoseStamped() #message is an instance of class PosStamped
+    msg.header.stamp = rospy.Time.now() #time stamp for msg
+    msg.header.frame_id = "world" #frame of message
+    msg.pose.position.x = 6.67
+    msg.pose.position.y = 6.67
+    msg.pose.position.z = 6.67
+    msg.pose.orientation.w = 1.0
+    pub.publish(msg)
+
+
 
     rospy.spin()
 
